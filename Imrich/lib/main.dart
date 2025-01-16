4491586753de856a34e6a6e5133650d62926779b
Huy Ph <huypmg.16@gmail.com>
2025-01-16 20:54:45 +0700

    I'am rich

---
+++ Imrich/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+            child: Scaffold(
+                appBar: AppBar(
+                  backgroundColor: Colors.black38,
+                  title: const Text("I'm super richhhhhh"),
+                  centerTitle: true,
+                ),
+                body: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage("images/diamond.png"),
+                      )
+                    ]))));
+  }
+}
